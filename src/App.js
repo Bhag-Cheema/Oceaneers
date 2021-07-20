9dc8f300fd026e6ef8f49782f0d4f47eae7d9887
Tasawar Saraf <tazsaraf@gmail.com>
2021-07-20 12:33:49 -0400

    navbar completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react'
 import Home from './components/Home';
 import Login from './components/Login';
+import "./styles/home.scss";
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { BrowserRouter, Route } from 'react-router-dom';
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,52 +1,28 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import logo from "../imgs/logo.png";
 
 export default class NavBar extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <div className="container-fluid d-flex justify-content-between">
-                        <a className="navbar-brand" href="#">Navbar</a>
+                <header className="main-head">
+                    <nav>
+                        <img id="logo" src={logo} alt="logo" />
+                        <ul>
+                            <li><NavLink to="/" activeClassName="active-link">Home</NavLink> </li>
+                            <li><NavLink to="" activeClassName="active-link">Why</NavLink></li>
+                            <li><NavLink to="" activeClassName="active-link">About Us</NavLink></li>
+                            <li><NavLink to="" activeClassName="active-link">Events</NavLink></li>
+                            <li><NavLink to="" activeClassName="active-link">Partners</NavLink></li>
+                            <li><NavLink to="" activeClassName="active-link">Blog</NavLink></li>
+                            <li><button className="join">Join Us</button></li>
+                            <li><button className="login">Log In</button></li>
+                            <li><i class="bi bi-search pointer"></i></li>
 
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarNav">
-                            <ul className="navbar-nav">
-                                <li className="nav-item">
-                                    <Link className="navbar-brand" to="/about">About</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Why</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">About Us</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Events</a>
-
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Partners</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Blog</a>
-                                </li>
-                                <li>
-                                    <button type="button" class="btn btn-primary">Join Us</button>
-                                </li>
-                                <li>
-                                    <Link className="navbar-brand" to="/about">Login</Link>
-                                </li>
-                                <li>
-                                    <i class="bi bi-search"></i>
-                                </li>
-
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
+                        </ul>
+                    </nav>
+                </header>
             </div>
         )
     }
