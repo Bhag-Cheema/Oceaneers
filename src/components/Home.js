25db7a4f02dbdb677ef5831dbcf13dfb3e580e0d
bhagcheema <bhagkcheema@gmail.com>
2021-07-24 13:06:14 -0700

    added join us button, as well as dropdown accordian

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,28 +5,30 @@ import image1 from "../imgs/image1.jpg";
 import image2 from "../imgs/image2.png";
 import image3 from "../imgs/image3.png";
 import image4 from "../imgs/image4.png";
-import "../styles/homepage.css";
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         <Navbar />
-        <div className="card bg-dark text-white">
+        <div className="card text-white border-0">
           <img id="iggy" src={beach} alt="beach" />
           <div className="card-img-overlay d-flex justify-content-center flex-column align-item-center text-center">
-            <h1 className="oceaneers-cabin">Oceaneers</h1>
-            <p className="card-text"> To the Oceans, Our Food Choices Matter</p>
+            <h1 className="oceaneers-cabin-large">Oceaneers</h1>
+            <p className="oceaneers-cabin-small"> To the Oceans, Our Food Choices Matter.</p>
+          </div>
+          <div className="d-flex justify-content-center">
+            <button className="learn-how">Want to Join? Learn how.</button>
           </div>
         </div>
 
         <div className="homepage-color">
           <div className="card-body justify-content-center text-center">
-            <h1 className="oceaneers-canbin">
+            <h1 className="oceaneers-cabin">
               oceaneers (n.) \\\\ˈō-shən–nirs \\\\
             </h1>
-            <div className="oceaneers-font">
-              <p className="card-text justify-content-center text-center">
+            <div className="oceaneers-raleway">
+              <p className="oceaneers-raleway">
                 A community of Ocean-aficionados who are driven by shared
                 passion for being in the ocean and a shared purpose of eating
                 for the ocean
@@ -36,7 +38,7 @@ export default class Home extends Component {
         </div>
 
         <div className="our-purpose">
-          <h1 className="card-body text-center">Our Purpose</h1>
+          <h1 className="our-purpose">Our Purpose</h1>
           <p className="our-purpose-text">
             Our Purpose is to take better care of our Oceans (and the living
             planet as a whole). We belive that our food choices play a huge role
@@ -45,89 +47,98 @@ export default class Home extends Component {
           </p>
         </div>
 
-        <div className="accordion">
-          <div className="accordion-item">
-            <h2 className="accordion-header" id="headingOne">
-              <button
-                className="accordion-button"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseOne"
-                aria-expanded="true"
-                aria-controls="collapseOne"
-              >
-                Awarness
-              </button>
-            </h2>
-            <div
-              id="collapseOne"
-              className="accordion-collapse collapse show"
-              aria-labelledby="headingOne"
-              data-bs-parent="#accordionExample"
-            >
-              <div className="accordion-body">
-                …is what we raise! Food choices matter more than most people
+        <div className="accordion" id="accordionExample">
+                  <div className="accordion-item">
+                    <h2 className="accordion-header justify-content-center" id="headingOne">
+                      <button
+                        class="button-accordian"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapseOne"
+                        aria-expanded="true"
+                        aria-controls="collapseOne"
+                      >
+                          Awarness
+                        <i className="bi bi-plus-circle"></i>
+                      </button>
+                    </h2>
+                    <div
+                      id="collapseOne"
+                      className="accordion-collapse collapse show"
+                      aria-labelledby="headingOne"
+                      data-bs-parent="#accordionExample"
+                    >
+                      <div className="accordion-body">
+                      …is what we raise! Food choices matter more than most people
                 (want to) know. That’s not a coincidence – there’s some pretty
                 powerful & loaded industries & lobbyists out there who want to
                 keep it that way
-              </div>
-            </div>
-          </div>
-          <div className="accordion-item">
-            <h2 className="accordion-header" id="headingTwo">
-              <button
-                className="accordion-button collapsed"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseTwo"
-                aria-expanded="false"
-                aria-controls="collapseTwo"
-              >
-                Empowerment
-              </button>
-            </h2>
-            <div
-              id="collapseTwo"
-              className="accordion-collapse collapse"
-              aria-labelledby="headingTwo"
-              data-bs-parent="#accordionExample"
-            >
-              <div className="accordion-body">
-                …is what drives us! 3 times a day you get to vote, with your
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+
+                  <div className="accordion" id="accordionExample">
+                  <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingOne">
+                      <button
+                        class="button-accordian"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapseOne"
+                        aria-expanded="true"
+                        aria-controls="collapseOne"
+                      >
+                          Empowerment
+                        <i className="bi bi-plus-circle"></i>
+                      </button>
+                    </h2>
+                    <div
+                      id="collapseOne"
+                      className="accordion-collapse collapse show"
+                      aria-labelledby="headingOne"
+                      data-bs-parent="#accordionExample"
+                    >
+                      <div className="accordion-body">
+                      …is what drives us! 3 times a day you get to vote, with your
                 hard-earned nuggets. Unless you’re munching like Iggy, then it’s
                 6-12 times. It’s like democracy, but on steroids.
-              </div>
-            </div>
-          </div>
-          <div className="accordion-item">
-            <h2 className="accordion-header" id="headingThree">
-              <button
-                className="accordion-button collapsed"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseThree"
-                aria-expanded="false"
-                aria-controls="collapseThree"
-              >
-                Reduction
-              </button>
-            </h2>
-            <div
-              id="collapseThree"
-              className="accordion-collapse collapse"
-              aria-labelledby="headingThree"
-              data-bs-parent="#accordionExample"
-            >
-              <div className="accordion-body">
-                ...is what we aim for! We don’t recommend to go from carnivore
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+
+                  <div className="accordion" id="accordionExample">
+                  <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingOne">
+                      <button
+                        class="button-accordian"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapseOne"
+                        aria-expanded="true"
+                        aria-controls="collapseOne"
+                      >
+                         Reduction
+                        <i className="bi bi-plus-circle"></i>
+                      </button>
+                    </h2>
+                    <div
+                      id="collapseOne"
+                      className="accordion-collapse collapse show"
+                      aria-labelledby="headingOne"
+                      data-bs-parent="#accordionExample"
+                    >
+                      <div className="accordion-body">
+                      ...is what we aim for! We don’t recommend to go from carnivore
                 to complete herbivore, the same way we don’t recommend to go
                 from Muizies to Dungeons (big wave spot). We believe that
                 reduction is the most sustainable path for most people.
-              </div>
-            </div>
-          </div>
-        </div>
-
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+                  
 <div className= "container">
 <div className="our-purpose">
           <h1 className="card-body text-center">Why become an Oceaneer?</h1> 
@@ -148,6 +159,7 @@ export default class Home extends Component {
         </div>
         </div>
       </div>
+    
     );
   }
 }
