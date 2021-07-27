771dd346da3d171012298e4169238815ce18e111
Tasawar Saraf <tazsaraf@gmail.com>
2021-07-27 16:52:37 -0400

    little tweaks for merge changes two

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,15 +10,15 @@ export default class NavBar extends Component {
                     <nav>
                         <img id="logo" src={logo} alt="logo" />
                         <ul>
-                            <li><NavLink to="/" activeClassName="active-link">Home</NavLink> </li>
-                            <li><NavLink to="/why" activeClassName="active-link">Why</NavLink></li>
-                            <li><NavLink to="" activeClassName="active-link">About Us</NavLink></li>
-                            <li><NavLink to="/events" activeClassName="active-link">Events</NavLink></li>
-                            <li><NavLink to="/partners" activeClassName="active-link">Partners</NavLink></li>
-                            <li><NavLink to="" activeClassName="active-link">Blog</NavLink></li>
+                            <li><NavLink to="/" activeStyle={{ color: "red" }} className="active-link">Home</NavLink> </li>
+                            <li><NavLink to="/why" activeStyle={{ color: "red" }} className="active-link">Why</NavLink></li>
+                            <li><NavLink to="" activeStyle={{ color: "red" }} className="active-link">About Us</NavLink></li>
+                            <li><NavLink to="/events" activeStyle={{ color: "red" }} className="active-link">Events</NavLink></li>
+                            <li><NavLink to="/partners" activeStyle={{ color: "red" }} className="active-link">Partners</NavLink></li>
+                            <li><NavLink to="" activeStyle={{ color: "red" }} className="active-link">Blog</NavLink></li>
                             <li><button className="join">Join Us</button></li>
                             <li><button className="login">Log In</button></li>
-                            <li><i class="bi bi-search pointer"></i></li>
+                            <li><i className="bi bi-search pointer"></i></li>
 
                         </ul>
                     </nav>
