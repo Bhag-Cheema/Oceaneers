87ee3ea55c9f7b66a927deb34b3e95be9d0e9875
Tasawar Saraf <tazsaraf@gmail.com>
2021-08-04 11:03:17 -0400

    Navbar and Newsletter update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,24 +3,73 @@ import { NavLink } from 'react-router-dom';
 import logo from "../imgs/logo.png";
 
 export default class NavBar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            clicked: false,
+        }
+
+    }
+
+
+
+    handleClickedMenu = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
     render() {
         return (
             <div>
                 <header className="main-head">
-                    <nav>
-                        <img id="logo" src={logo} alt="logo" />
-                        <ul>
-                            <li><NavLink to="/" activeStyle={{ color: "red" }} className="active-link">Home</NavLink> </li>
-                            <li><NavLink to="/why" activeStyle={{ color: "red" }} className="active-link">Why</NavLink></li>
-                            <li><NavLink to="/aboutus" activeStyle={{ color: "red" }} className="active-link">About Us</NavLink></li>
-                            <li><NavLink to="/events" activeStyle={{ color: "red" }} className="active-link">Events</NavLink></li>
-                            <li><NavLink to="/partners" activeStyle={{ color: "red" }} className="active-link">Partners</NavLink></li>
-                            <li><NavLink to="" activeStyle={{ color: "red" }} className="active-link">Blog</NavLink></li>
-                            <li><button className="join">Join Us</button></li>
-                            <li><button className="login">Log In</button></li>
-                            <li><i className="bi bi-search pointer-why"></i></li>
-
-                        </ul>
+                    <nav className="navbar navbar-expand-lg navbar-dark">
+                        <div className="m-2 ">
+                            <img src={logo} alt="Logo" height="40" />
+                        </div>
+                        <button
+                            className="navbar-toggler me-2"
+                            id="hamburger"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#navbarNav"
+                            aria-controls="navbarNav"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse ms-2" id="navbarNav">
+                            <ul className="navbar-nav ms-auto">
+                                <li className="nav-item">
+                                    {/* <a className="nav-link" href="">Home</a> */}
+                                    <NavLink className="nav-link" to="/">Home</NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink className="nav-link" to="/why">Why</NavLink>
+                                </li>
+                                <li className="nav-item ">
+                                    <NavLink className="nav-link" to="/aboutus">About Us</NavLink>
+                                </li>
+                                <li className="nav-item ">
+                                    <NavLink className="nav-link" to="/events">Events</NavLink>
+                                </li>
+                                <li className="nav-item ">
+                                    <NavLink className="nav-link" to="/partners">Partners</NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink className="nav-link" to="/">Blog</NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <button className="join">Join Us</button>
+                                </li>
+                                <li className="nav-item">
+                                    <button className="login">Login</button>
+                                </li>
+                                <li className="nav-item">
+                                    <i className="bi bi-search pointer-why"></i>
+                                </li>
+                            </ul>
+                        </div>
                     </nav>
                 </header>
             </div>
